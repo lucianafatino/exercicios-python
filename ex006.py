1b0ad49caa248432a7b96dac3b4d33e5b754355a
Luciana Venancio <lucianavenancio54@gmail.com>
2021-11-15 13:58:08 -0300

    Exercicio corrigido

--- ex006.py
+++ ex006.py
@@ -1,6 +1,3 @@
 # Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada
 n1 = int(input('Digite um número: '))
-dob = n1 * 2
-tri = n1 * 3
-rq = n1 ** 2
-print('O dobro de {} é {}, o triplo é {} e a raiz quadrada é {}'.format(n1, dob, tri, rq))
+print('O dobro de {} é {} \nO triplo é {} \nSua raiz quadrada é {:.2f}'.format(n1, (n1 * 2), (n1 * 3), (n1 ** (1/2))))
