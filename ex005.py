8a85ae5526adb768b8da12a7a4a0da100a8d8fa1
Luciana Venancio <lucianavenancio54@gmail.com>
2021-11-15 13:41:13 -0300

    Exercício corrigido

--- ex005.py
+++ ex005.py
@@ -3,3 +3,5 @@ n1 = int(input('Digite um número: '))
 suc = n1 + 1
 ant = n1 - 1
 print('O sucessor de {} é {} e o antecessor é {}'. format(n1, suc, ant))
+#ou você pode retirar as variáveis suc e ant e escrever o print da seguinte forma:
+#print('O sucessor de {} é {} e o antecessor é {}'. format(n1, (n1-1), (n1+)))
