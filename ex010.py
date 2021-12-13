7b1a953408bb1513283e0dc42776ca3ff7ed6540
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-13 11:05:31 -0300

    Exercicio corrigido

--- ex010.py
+++ ex010.py
@@ -1,7 +1,8 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
-# Considere que U$ 1,00 = R$ 5,54
+# Considere que U$ 1,00 = R$ 5,63 e EUR = R$ 6,35
 
-realm = float(input('Quantos reais você deseja converter para dólares? '))
-dolarm = float(input('Favor digitar a cotação atual do dólar: '))
-realcon = realm / dolarm
-print('O valor R$ {} convertido para dólares é U$ {:.2f}'.format(realm, realcon))
+
+real = float(input('Quantos reais você deseja converter? R$ '))
+dolar = real / 5.63
+euro = real / 6.35
+print('O valor de R$ {:.2f} em dólares é de USD {:.2f}, e em euros é de EUR {:.2f}'.format(real, dolar, euro))
