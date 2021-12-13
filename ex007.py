b26ed98f7f29ce65f606d7b9c24a56a8b6db5ed2
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-13 09:50:24 -0300

    Exercicio corrigido

--- ex007.py
+++ ex007.py
@@ -1,5 +1,5 @@
 # Desenvolva um programa que leia duas notas de um aluno, calcule e mostre sua média
-notaum = int(input('Digite a primeira nota: '))
-notadois = int(input('Digite a segunda nota: '))
+notaum = float(input('Digite a primeira nota: '))
+notadois = float(input('Digite a segunda nota: '))
 media = (notaum + notadois) / 2
-print('A média do aluno é {:.2f}'.format(media))
+print('A média do aluno é {:.1f}'.format(media))
