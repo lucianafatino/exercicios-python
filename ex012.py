18c5402c45533b283001dfc5f58078ebee113f53
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-13 13:54:23 -0300

    Exercicio corrigido

--- ex012.py
+++ ex012.py
@@ -1,5 +1,4 @@
 # Faça um algoritimo que leia o preço de um produto e mostre seu novo preço, com 5 % de desconto
-n1 = float(input('Digite o valor do produto: '))
-desconto = (5 * n1) / 100
-valor = n1 - desconto
-print('O valor, com desconto de 5%, a ser pago será de R$ {:.2f}'.format(valor))
+n1 = float(input('Digite o valor do produto: R$ '))
+desconto = n1 - (n1 * 5 / 100)
+print('O produto que custava R$ {}, na promoção com desconto de 5% vai custar R$ {:.2f}'.format(n1, desconto))
