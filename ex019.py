6c2b9560e72ecdf565bf1d0fc7db3935415384ca
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-20 14:56:55 -0300

    Exercicio corrigido

--- ex019.py
+++ ex019.py
@@ -1,1 +1,11 @@
-# Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+# Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo
+# o nome do escolhido.
+from random import choice
+# a1, a2, a3, a4 = 'Luciana', 'Mariana', 'Júlia', 'Marcos'
+primeiro = input('Digite o nome do primeiro aluno: ')
+segundo = input('Digite o nome do segundo aluno: ')
+terceiro = input('Digite o nome do terceiro aluno: ')
+quarto = input('Digite o nome do quarto aluno: ')
+lista = [primeiro, segundo, terceiro, quarto]
+print('O sorteado(a) foi {}'.format(choice(lista)))
+
