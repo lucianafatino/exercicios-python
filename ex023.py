173786f3253cd6398dbf2aaf03ff4b7c6d7439a6
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-21 15:47:28 -0300

    Exercicios por corrigir

---
+++ ex023.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
+# Exemplo:
+    # Digite um número: 1895
+    # unidade: 5
+    # dezena: 9
+    # centena: 8
+    # milhar: 1
