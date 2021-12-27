140902bcdd31d84936d55866a416c52d786ae21b
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-27 18:05:16 -0300

    Exercicio corrigido

--- ex023.py
+++ ex023.py
@@ -1,7 +1,17 @@
 # Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
 # Exemplo:
-    # Digite um número: 1895
-    # unidade: 5
-    # dezena: 9
-    # centena: 8
-    # milhar: 1
+# Digite um número: 1895
+# unidade: 5
+# dezena: 9
+# centena: 8
+# milhar: 1
+numero = int(input('Digite um número: '))
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+print('Analisando o número {}...'.format(numero))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
