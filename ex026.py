80da346c6556ea56b585e78b2b40af45f0ef01ea
Luciana Venancio <lucianavenancio54@gmail.com>
2021-12-28 18:25:40 -0300

    Exercicio corrigido

--- ex026.py
+++ ex026.py
@@ -2,3 +2,7 @@
 # Quantas vezes aparece a letra 'A'
 # Em que posição ela aparece a primeira vez
 # Em que posição ela aparece pela última vez
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A aparece {} vezes'.format(frase.count('A')))
+print('A letra A aparece pela primeira vez na posição {}'.format(frase.find('A')+1))
+print('A letra A aparece pela últmia vez na posição {}'.format(frase.rfind('A')+1))
