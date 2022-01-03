d9c62b0d79b016019440901f317ca7a0dcc9fd2a
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-03 11:35:47 -0300

    Exercicio por resolver

---
+++ ex029.py
@@ -0,0 +1,3 @@
+# Escreva um programa que leia a velocidade de um carro
+# Se ele ultrapassar 80 km/h, mostre um msg dizendo que ele foi multado
+# A multa vai custar R$ 7 por cada km acima do limite
