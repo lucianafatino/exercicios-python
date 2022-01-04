09b836dda40caf3198ecea1dc8494d4fbdf640f9
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-04 15:11:57 -0300

    Exercicio corrigido

--- ex029.py
+++ ex029.py
@@ -1,3 +1,8 @@
 # Escreva um programa que leia a velocidade de um carro
 # Se ele ultrapassar 80 km/h, mostre um msg dizendo que ele foi multado
 # A multa vai custar R$ 7 por cada km acima do limite
+vel = float(input('Digite a velocidade do seu carro: '))
+if vel > 80:
+    multa = (vel - 80) * 7
+    print('Ultrapassou a velocidade máxima de 80KM/h! Você foi multado em R$ {:.2f}'.format(multa))
+print('Tudo em ordem! Dirija com segurança!')
