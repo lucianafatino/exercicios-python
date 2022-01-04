32509c406ca5ae3276e36e5b7c83b8295d8af23f
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-04 15:37:09 -0300

    Exercicio corrigido

--- ex030.py
+++ ex030.py
@@ -1,1 +1,7 @@
 # Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR
+numero = int(input('Digite um número inteiro: '))
+resultado = numero % 2
+if resultado == 0:
+    print('O número {} é Par'.format(numero))
+else:
+    print('O número {} é Ímpar'.format(numero))
