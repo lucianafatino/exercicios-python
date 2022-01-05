4a4e216e8f5b03d02471c77e8a4a3177298ecdc5
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-05 11:13:13 -0300

    Exercicio corrigido

--- ex035.py
+++ ex035.py
@@ -1,2 +1,14 @@
-# Desenvolva um progrma que leia o comprimento de três retas e diga ao usuário
+# Desenvolva um programa que leia o comprimento de três retas e diga ao usuário
 # se elas podem ou não formar um triângulo
+print('-=' * 15)
+print('Analisador de Triângulos')
+print('-=' * 15)
+a = float(input('Digite o valor da primeira reta: '))
+b = float(input('Digite o valor da segunda reta: '))
+c = float(input('Digite o valor da terceira reta: '))
+if a < b + c and b < a + c and c < a + b:
+    print('Os segmentos acima PODEM formar um triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM formar um triângulo!')
+
+
