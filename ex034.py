0cf9e0a2e6e7a11fc97ab72597b75b2630a9c990
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-05 10:49:14 -0300

    Exercicio corrigido

--- ex034.py
+++ ex034.py
@@ -1,3 +1,10 @@
 # Escreva um programa que pergunte o salario de um funcionário e calcule o valor de seu aumento
 # Para salários superiores a R$ 1.250,00, calcule um aumento de 10%
 # Para inferiores ou iguais, o aumento é de 15%
+salario = float(input('Qual é o seu salário? R$ '))
+if salario <= 1250:
+    novo = salario + (salario * 15 / 100)
+    print('Seu novo salário, com reajuste de 15% passará a ser R$ {:.2f}'.format(novo))
+else:
+    novo = salario + (salario * 10 / 100)
+    print('Seu novo salário, com reajuste de 10% será de R$ {:.2f}'.format(novo))
