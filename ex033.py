330eb009c3191c8ef91bb2ad03e7fdf11e85eadf
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-05 10:37:01 -0300

    Exercicio corrigido

--- ex033.py
+++ ex033.py
@@ -1,1 +1,20 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o MENOR
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+c = int(input('Digite o terceiro número: '))
+# testando o menor valor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+# testando o maior valor
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor é {}'.format(menor))
+print('O maior valor é {}'.format(maior))
+
+
