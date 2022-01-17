a8e22ed865e96426946f11e9561a04ba26e31ec0
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-17 11:22:42 -0300

    Exercicio por resolver

---
+++ ex036.py
@@ -0,0 +1,4 @@
+''' Escreva um programa para aprovar um empréstimo bancário para a compra de uma casa.
+O programa vai perguntar o VALOR DA CASA, o SALÁRIO do comprador e em QUANTOS ANOS ele vai pagar.
+Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário, ou então o empréstimo
+será negado '''
