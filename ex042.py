5630425f3b43b4c5d3d7cc31e2b0bea57c14a45a
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-17 11:28:07 -0300

    Exercicio por resolver

---
+++ ex042.py
@@ -0,0 +1,5 @@
+''' Refaça o desafio 035, dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+    - EQUILÁTERO: Todos os lados iguais
+    - ISÓSCELES: Dois lados iguais
+    - ESCALENO: Todos os lados diferentes
+'''
