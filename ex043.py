d2403f873dbfc32169545d2decd53dd7b28430f5
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-17 11:28:37 -0300

    Exercicio por resolver

---
+++ ex043.py
@@ -0,0 +1,8 @@
+'''Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule o seu IMC e mostre o status, de
+acordo com a tabela abaixo:
+    - Abaixo de 18.5: ABAIXO DO PESO
+    - Entre 18.5 e 25: PESO IDEAL
+    - 25 até 30: SOBREPESO
+    - 30 até 40: OBESIDADE
+    - Acima de 40: OBESIDADE MÓRBIDA
+'''
