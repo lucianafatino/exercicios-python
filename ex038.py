72b66d155dee364cc520d06d797aaeb9fbc42fae
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-17 11:25:57 -0300

    Exercicio por resolver

---
+++ ex038.py
@@ -0,0 +1,5 @@
+'''Escreva um programa que leia DOIS NÚMEROS inteiros e compare-os, mostrando na tela a mensagem:
+    - O primeiro valor é maior
+    - O segundo valor é maior
+    - Não existe valor maior, os dois são iguais
+'''
