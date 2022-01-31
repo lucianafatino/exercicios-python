509249f50a6e6338bec60368ef2c7bf6976bf657
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-31 15:16:26 -0300

    Exercicio corrigido

--- ex038.py
+++ ex038.py
@@ -3,3 +3,11 @@
     - O segundo valor é maior
     - Não existe valor maior, os dois são iguais
 '''
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+if n1 > n2:
+    print('O primeiro valor - {} - é maior'.format(n1))
+elif n1 < n2:
+    print('O segundo valor - {} - é maior.'.format(n2))
+else:
+    print('Não existe valor maior, os dois são iguais.')
