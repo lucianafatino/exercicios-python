d99382ed63fe8b12cc524ac858b99ea30a6e6fab
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-31 15:56:26 -0300

    Exercicio corrigido

--- ex040.py
+++ ex040.py
@@ -4,3 +4,12 @@ no final, de acordo com a média atingida:
     - Média entre 5.0 e 6.9: RECUPERAÇÃO
     - Média de 7.0 ou superior: APROVADO
 '''
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+if media < 5:
+    print('Sua media foi {:.1f}. \033[1;31mREPROVADO!'.format(media))
+elif media <= 6.9:
+    print('Sua média foi {:.1f}. \033[1;33mRECUPERAÇÃO!'.format(media))
+else:
+    print('Sua média foi {:.1f}. \033[1;32mAPROVADO!!'.format(media))
