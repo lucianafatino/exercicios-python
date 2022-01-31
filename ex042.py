310487faff6bc4ca5b020ecbfeb86fb5b788add7
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-31 16:24:04 -0300

    Exercicio corrigido

--- ex042.py
+++ ex042.py
@@ -3,3 +3,16 @@
     - ISÓSCELES: Dois lados iguais
     - ESCALENO: Todos os lados diferentes
 '''
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos PODEM formar um triângulo.')
+    if r1 == r2 == r3:
+        print('EQUILÁTERO')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos NÃO PODEM formar um triângulo.')
