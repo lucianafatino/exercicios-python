6cfdfb58b9afe895b563054164212d6f497bde03
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-31 15:07:52 -0300

    Exercicio corrigido

--- ex037.py
+++ ex037.py
@@ -4,3 +4,25 @@ base de conversão:
 - 2 para octal
 - 3 para hexadecimal
 '''
+from time import sleep
+numUsuário = int(input('Digite um número inteiro: '))
+print('''Escolha uma das bases para conversão:
+[1] Converter para Binário
+[2] Converter para Octal
+[3] Converter para Hexadecimal
+''')
+pedeUsuário = int(input('Escolha o número correspondente à opção desejada: '))
+sleep(1)
+print('-=' * 25)
+if pedeUsuário == 1:
+    print('A base de conversão escolhida foi o Binário')
+    print('O número {} convertido para binário passa a ser {}'.format(numUsuário, bin(numUsuário)[2:]))
+elif pedeUsuário == 2:
+    print('A base de conversão escolhida foi o Octal')
+    print('O número {} convertido para octal passa a ser {}'.format(numUsuário, oct(numUsuário)[2:]))
+elif pedeUsuário == 3:
+    print('A base de conversão escolhida foi o Hexadecimal')
+    print('O número {} convertido para hexadecimal passa a ser {}'.format(numUsuário, hex(numUsuário)[2:]))
+else:
+    print('Opção Inválida')
+print('-=' * 25)
