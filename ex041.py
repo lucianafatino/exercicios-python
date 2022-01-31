04f2a5059a455b17d6a5a8c7d1bc7c2021ce901b
Luciana Venancio <lucianavenancio54@gmail.com>
2022-01-31 16:05:55 -0300

    Exercicio corrigido

--- ex041.py
+++ ex041.py
@@ -3,6 +3,21 @@ e mostre sua categoria, de acordo com a idadde:
     - Até 9 anos: MIRIM
     - Até 14 anos: INFANTIL
     - Até 19 anos: JUNIOR
-    - Até 20 anos: SÊNIOR
+    - Até 25 anos: SÊNIOR
     - Acima: MASTER
 '''
+from datetime import date
+atual = date.today().year
+ano = int(input('Digite seu ano de nascimento: '))
+idade = atual - ano
+print('O atleta tem {} anos.'.format(idade))
+if idade <= 9:
+    print('Categoria: MIRIM.')
+elif idade <= 14:
+    print('Categoria: INFANTIL.')
+elif idade <= 19:
+    print('Categoria: JUNIOR.')
+elif idade <= 25:
+    print('Categoria: SÊNIOR.')
+else:
+    print('Categoria: MASTER.')
