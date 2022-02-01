57550f321b052cc8adb00fbe819b639240ef0834
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-01 13:26:22 -0300

    Exercicio por fazer

---
+++ ex053.py
@@ -0,0 +1,8 @@
+'''Crie um programa que leia uma frase qualquer e diga se ela é um palídromo, desconsiderando os espaços
+*** Palíndromos são palavras que, se lidas de trás para frente e de frente para trás, serão a mesma coisa
+ex: apos a sopa
+    a sacada da casa
+    a torre da derrota
+    o lobo ama o bolo
+    anotaram a data da maratona
+'''
