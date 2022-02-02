e68337743fdc65be8fbfb1ad38951554720199ec
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-02 19:02:13 -0300

    Exercicio corrigido

--- ex049.py
+++ ex049.py
@@ -1,1 +1,4 @@
 '''Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora faça utilizando o laço for'''
+n = int(input('Digite um valor: '))
+for c in range(1, 11):
+    print('{} x {} = {}'.format(n, c, c*n))
