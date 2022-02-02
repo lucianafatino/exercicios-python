e6772eab2c44d6c03e81b5d17ab67a41762fde08
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-02 19:01:34 -0300

    Exercicio corrigido

--- ex047.py
+++ ex047.py
@@ -1,1 +1,12 @@
 '''Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50'''
+for c in range(1, 50+1): # faz duas iterações
+    if c % 2 == 0:
+        print(c, end=' ')
+print('FIM')
+
+# Resolução do prof
+for c in range(2, 51, 2): # faz menos iteração
+    print(c, end=' ')
+print('FIM')
+
+
