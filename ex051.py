0ce6ed7b1874e283dff37a9da27f4df1c960bebe
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-07 09:34:18 -0300

    Exercicio corrigido

--- ex051.py
+++ ex051.py
@@ -1,2 +1,13 @@
-'''Desenvolva um programa que leia o primeiro termo e a razao de uma PA (progressão aritimética. No final,
+'''Desenvolva um programa que leia o primeiro termo e a razao de uma PA (progressão aritimética). No final,
 mostre os 10 primeiros termos dessa progressão'''
+print('=' * 30)
+print('10 TERMOS DE UMA PA'.center(30))
+print('=' * 30)
+
+inicio = int(input("Primeiro elemento: "))
+passo = int(input("Passo: "))
+fim = inicio + (10-1)*passo
+fim += 1
+for c in range(inicio, fim, passo):
+    print(c,end=' ↠ ')
+print('ACABOU')
