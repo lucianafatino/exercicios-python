18fa66020f388fa37efd2d29c1b868603d0e5378
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-07 12:39:33 -0300

    Exercicio corrigido

--- ex053.py
+++ ex053.py
@@ -6,3 +6,15 @@ ex: apos a sopa
     o lobo ama o bolo
     anotaram a data da maratona
 '''
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+# inverso = junto[::-1] // tirando o for, podemos usar esta forma simplificada de verificar palíndromos
+for letra in range(len(junto)-1, -1, -1):
+    inverso += junto[letra]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('É um palíndromo')
+else:
+    print('Não é um palíndromo')
