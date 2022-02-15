6ca8f1e33928de29f3815af748c424e3583f7bec
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-15 16:18:08 -0300

    Exercicio corrigido

--- ex065.py
+++ ex065.py
@@ -3,3 +3,20 @@ Crie um programa que leia vários números inteiros pelo teclado. No final da ex
 média entre todos os valores e qual foi o maior e o menor valor lido. O programa deve perguntar
 ao usuário se ele quer continuar ou não a digitar mais valores.
 '''
+resposta = 'S'
+soma = qtd = media = maior = menor = 0
+while resposta in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    qtd += 1
+    if qtd == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resposta = str(input('Quer continuar [S/N]? ')).upper().strip()[0]
+media = soma / qtd
+print('Você digitou {} números e a média foi {}'.format(qtd, media))
+print('O menor número foi {} e maior número foi {}.'.format(menor, maior))
