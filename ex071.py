c9d34cb670e613b631e88b02528a377b5309993b
Luciana Venancio <lucianavenancio54@gmail.com>
2022-02-28 14:27:25 -0300

    Exercicio por resolver

---
+++ ex071.py
@@ -0,0 +1,6 @@
+'''
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será
+o valor a ser sacado (int) e o programa vai informar quantas cédulas de cada valor serão
+entreges.
+OBS: Considere que o caixa possui cédulas de 50, 20, 10 e 1.
+'''
