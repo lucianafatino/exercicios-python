c7b45884f68110cd00e2117b1e91bf12c18c4644
Luciana Venancio <lucianavenancio54@gmail.com>
2022-04-05 18:23:00 -0300

    Exercicio corrigido

--- ex067.py
+++ ex067.py
@@ -2,3 +2,12 @@
 Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
 O programa será interrompido quando o número solicitado for um número negativo.
 '''
+while True:
+    print('-' * 40)
+    tab = int(input('Quer ver a tabuada de qual valor? '))
+    print('-' * 40)
+    if tab < 0:
+        break
+    for c in range(1,11):
+        print(f'{c} x {tab} = {c*tab}')
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre!')
