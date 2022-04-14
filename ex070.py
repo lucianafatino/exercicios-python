f659afa21f2702f34b6becc9ab57c550932616a4
Luciana Venancio <lucianavenancio54@gmail.com>
2022-04-14 10:11:44 -0300

    Exercicio corrigido

--- ex070.py
+++ ex070.py
@@ -5,3 +5,24 @@ A) Qual é o total gasto na compra
 B) Quantos produtos custam mais de R$ 1000,00
 C) Qual é o nome do produto mais barato
 '''
+
+totalgasto = prod100 = maisbarato = cont = 0
+barato = ''
+while True:
+    askproduto = str(input('Nome do produto: ')).strip()
+    askpreço = float(input('Preço do Produto: R$ '))
+    cont += 1
+    totalgasto += askpreço
+    if askpreço > 1000:
+        prod100 += 1
+    if cont == 1 or askpreço < maisbarato:
+        maisbarato = askpreço
+        barato = askproduto
+    asksn = ' '
+    while asksn not in 'SN':
+        asksn = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if asksn == 'N':
+        break
+print(f'O total gasto foi de R$ {totalgasto:.2f}')
+print(f'Temos {prod100} produto custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {barato}, que custa R$ {maisbarato:.2f}')
