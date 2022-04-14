dc3d9849fb1c8378d1d292b95f8392bb42dc22ff
Luciana Venancio <lucianavenancio54@gmail.com>
2022-04-14 10:27:06 -0300

    Exercicio corrigido

--- ex071.py
+++ ex071.py
@@ -4,3 +4,24 @@ o valor a ser sacado (int) e o programa vai informar quantas cédulas de cada va
 entreges.
 OBS: Considere que o caixa possui cédulas de 50, 20, 10 e 1.
 '''
+valor = float(input('Que valor você quer sacar? R$'))
+total = valor
+céd = 50
+totcéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédulas de R$ {céd}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+print('Volte sempre!')
