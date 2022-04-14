761ca0738a84181019e69183ae219574da8feeb4
Luciana Venancio <lucianavenancio54@gmail.com>
2022-04-14 09:32:44 -0300

    Exercicio corrigido

--- ex068.py
+++ ex068.py
@@ -1,4 +1,56 @@
 '''
-Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando
-o total de vitórias consecutivas que ele conquistou ao final do jogo.
+Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder,
+mostrando o total de vitórias consecutivas que ele conquistou ao final do jogo.
 '''
+from random import randint
+from time import sleep
+
+print('=-' * 15)
+print('VAMOS JOGAR PAR OU IMPAR')
+print('=-' * 15)
+
+v = 0
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0,10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PpIi':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador jogou {computador}. Total de {total}. ', end='')
+    print(f'Deu PAR' if total % 2 == 0 else 'Deu ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print('Vamos jogar novamente')
+print(f'GAME OVER! Você venceu {v} vezes!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
