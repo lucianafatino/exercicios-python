b80cc308790754dd07e3d30f92be6159fdb115bf
Luciana Venancio <lucianavenancio54@gmail.com>
2022-04-14 09:50:23 -0300

    Exercicio corrigido

--- ex069.py
+++ ex069.py
@@ -5,3 +5,45 @@ A) Quantas pessoas tem mais de 18 anos
 B) Quantos homens foram cadastrados
 C) Quantas mulheres tem menos de 20 anos
 '''
+
+# Minha solução
+mais18 = masc = fem20 = 0
+
+while True:
+    askidade = int(input('Idade: '))
+    asksexo = str(input('Gênero [M/F]: ')).strip().upper()[0]
+    sn = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if askidade > 18:
+        mais18 += 1
+    if asksexo == 'M':
+        masc += 1
+    if askidade < 20 and asksexo == 'F':
+        fem20 += 1
+    if sn == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {mais18}')
+print(f'Ao todo temos {masc} homens cadastrados')
+print(f'E temos {fem20} mulheres com menos de 20 anos.')
+
+# Solução do Prof
+mais18 = masc = fem20 = 0
+
+while True:
+    askidade = int(input('Idade: '))
+    asksexo = ' '
+    sn = ' '
+    while asksexo not in 'MF':
+        asksexo = str(input('Gênero [M/F]: ')).upper()[0]
+    if askidade >= 18:
+        mais18 += 1
+    if asksexo == 'M':
+        masc += 1
+    if askidade < 20 and asksexo == 'F':
+        fem20 += 1
+    while sn not in 'SN':
+        sn = str(input('Deseja continuar? [S/N] ')).upper()[0]
+    if sn == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {mais18}')
+print(f'Ao todo temos {masc} homens cadastrados')
+print(f'E temos {fem20} mulheres com menos de 20 anos.')
